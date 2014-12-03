751eb1249e2fc532442a1a75df4d217b83542959
johnnyjoyce <g00283925@gmit.ie>
2014-12-03 15:34:01 +0000

    Again testing

--- DistributedSystemsFibonacci/src/ie/gmit/Runner.java
+++ DistributedSystemsFibonacci/src/ie/gmit/Runner.java
@@ -3,7 +3,7 @@ package ie.gmit;
 public class Runner {
 	public static void main(String[] args) {
 		System.out.println("Test");
-		
+		System.out.println("Dunning");
 		System.out.println("Another Test");
 	}
 
